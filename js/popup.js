fc0a96724562eb9862273ba7e6c84c1bd188ebd5
Mary Adeke <maryadeke9@gmail.com>
2024-04-15 14:58:47 +0300

    trying to use php

--- js/popup.js
+++ js/popup.js
@@ -136,3 +136,25 @@ function toggle3() {
 }
 
 
+// Add event listeners to handle search input events. When the user types a query, the function performSearch() should be triggered to make an AJAX request.
+function showResult(str) {
+    if (str.length==0) {
+      document.getElementById("livesearch").innerHTML="";
+      document.getElementById("livesearch").style.border="0px";
+      return;
+    }
+    var xmlhttp=new XMLHttpRequest();
+    xmlhttp.onreadystatechange=function() {
+      if (this.readyState==4 && this.status==200) {
+        document.getElementById("livesearch").innerHTML=this.responseText;
+        document.getElementById("livesearch").style.border="1px solid #A5ACB2";
+      }
+    }
+    xmlhttp.open("GET","livesearch.php?q="+str,true);
+    xmlhttp.send();
+  }
+
+
+
+
+  
